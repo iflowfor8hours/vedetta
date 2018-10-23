a2981f7b182f8bf6d0a8ed5e2abe7043a3ae6e9c
matt urbanski <murbanski@apple.com>
2018-10-23 01:47:11 -0700

    rename all boot.vedetta instances

--- template.sh
+++ template.sh
@@ -5,8 +5,10 @@ wan_em0
 lan_em1=172.16.99.1
 lan_em2=192.168.99.1
 
-# All the direct references go first
+# All the direct references and aliases are safe
 sed -i 's/acolyte.vedetta.lan/${host}.${domain}.${tld}/g' $(git ls-files)
+sed -i 's/boot.vedetta.lan/boot.${domain}.${tld}/g' $(git ls-files)
+
 
 sed -i 's/vedetta.lan/${domain}.${tld}/g' $(git ls-files)
 sed -i 's/vedetta/${domain}/g' $(git ls-files)
